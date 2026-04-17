10fdfc2d9deaeca855305fd72f10ce8100600dfd
MovelikeLo <loicrobert640@gmail.com>
2026-04-17 13:25:29 -0400

    Init

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import { CORE_CONCEPTS } from './data';
+import Header from './components/Header/Header';
+import CoreConcept from './components/CoreConcepts/CoreConcepts';
+// function UserData(){
+//   const user = {first:"George",last:"Salayka", age:"40" };
+
+//   return (
+//     <div>
+//       <h2>Name: {user.last}, {user.first}</h2>
+//       <h3>Age: {user.age}</h3>
+//     </div>
+//   )
+// }
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <main>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept
+              {
+              ...CORE_CONCEPTS[0]
+              }
+            />
+            <CoreConcept
+              {
+              ...CORE_CONCEPTS[1]
+              }
+            />
+            <CoreConcept
+              {
+              ...CORE_CONCEPTS[2]
+              }
+            />
+            <CoreConcept
+              {
+              ...CORE_CONCEPTS[3]
+              }
+            />
+            {
+              // CORE_CONCEPTS.map((concept)=><CoreConcept {...concept}/>)
+            }
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default App;
