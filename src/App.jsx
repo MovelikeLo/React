97b36cfa219913d02e375486841de04fb0d84159
MovelikeLo <loicrobert640@gmail.com>
2026-04-21 14:36:01 -0400

    added useState and handle select

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
-import { CORE_CONCEPTS } from './data';
+import { CORE_CONCEPTS, EXAMPLES} from './data';
 import Header from './components/Header/Header';
 import CoreConcept from './components/CoreConcepts/CoreConcepts';
 import TabButton from './components/TabButton/TabButton';
+import { use, useState } from 'react';
 // function UserData(){
 //   const user = {first:"George",last:"Salayka", age:"40" };
 
@@ -14,6 +15,25 @@ import TabButton from './components/TabButton/TabButton';
 // }
 
 function App() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  console.log("app is rendered");
+  let tabContent = <p>Please select a tab</p>;
+if (selectedTopic) {
+  tabContent = (
+    <div>
+      <h3>{EXAMPLES[selectedTopic].title}</h3>
+      <p>{EXAMPLES[selectedTopic].description}</p>
+      <pre>
+        <code>{EXAMPLES[selectedTopic].code}</code>
+      </pre>
+    </div>
+  );
+}
+  
+  function handleSelect(selectedButton) {
+    setSelectedTopic(selectedButton);
+    console.log(selectedTopic);
+  }
   return (
     <div>
       <Header />
@@ -49,11 +69,12 @@ function App() {
         <section id="examples">
           <h2>Examples</h2>
           <menu>
-            <TabButton>Components</TabButton>
-            <TabButton>JSX</TabButton>
-            <TabButton>Props</TabButton>
-            <TabButton>State</TabButton>
+            <TabButton onSelect={() => handleSelect('Components')}>Components</TabButton>
+            <TabButton onSelect={() => handleSelect('JSX')}>JSX</TabButton>
+            <TabButton onSelect={() => handleSelect('Props')}>Props</TabButton>
+            <TabButton onSelect={() => handleSelect('State')}>State</TabButton>
           </menu>
+            {tabContent}
         </section>
       </main>
     </div>
